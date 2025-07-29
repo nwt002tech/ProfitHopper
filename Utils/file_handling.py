091fee0e6448c7b1e10fb8a62c589255425bc80d
nwt002tech <56565545+nwt002tech@users.noreply.github.com>
2025-07-28 20:14:25 -0500

    Add files via upload

---
+++ Utils/file_handling.py
@@ -0,0 +1,9 @@
+# utils/file_handling.py
+import base64
+import pandas as pd
+
+# CSV download helper
+def get_csv_download_link(df, filename):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    return f'<a href="data:file/csv;base64,{b64}" download="{filename}">Download CSV</a>'
