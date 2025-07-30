d6dca9670461c9982808c090cbd55434d9c81841
nwt002tech <56565545+nwt002tech@users.noreply.github.com>
2025-07-30 11:54:33 -0500

    Delete src directory

--- src/utils/file_handling.py
+++
@@ -1,7 +0,0 @@
-import base64
-import pandas as pd
-
-def get_csv_download_link(df, filename):
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()
-    return f'<a href="data:file/csv;base64,{b64}" download="{filename}">Download CSV</a>'
